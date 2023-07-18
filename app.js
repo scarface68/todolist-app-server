e0108d8df367ef89a90ee0c7d9b9526c279eb037
Sai Chandan Mohanty <saichandan68@gmail.com>
2023-07-18 16:50:46 +0530

    port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
